61a43fd3d225605b9eaaafe6c419bb0137b81b75
rednafi <redowan.nafi@gmail.com>
2019-10-28 12:30:34 +0600

    added super robust logfunc

--- utils.py
+++ utils.py
@@ -1,34 +1,74 @@
 import functools
 import logging
-import sys 
+import sys
 import os
 
-# Create a custom logger
-logger = logging.getLogger(__name__)
 
-# Create handlers
-console_handler = logging.StreamHandler()
-file_handler = logging.FileHandler('logs.log')
+def create_logger(log_path: str = "logs/logs.log") -> logging.Logger:
 
-console_handler.setLevel(logging.DEBUG)
-file_handler.setLevel(logging.ERROR)
+    # create folder
+    if not os.path.exists(log_path):
+        os.mkdir("logs")
 
-# Create formatters and add it to handlers
-console_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-console_handler.setFormatter(console_format)
-file_handler.setFormatter(file_format)
+    # Create a custom logger
+    logger = logging.getLogger(__name__)
+
+    # Create handlers
+    console_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler(log_path)
+
+    console_handler.setLevel(logging.WARNING)
+    file_handler.setLevel(logging.ERROR)
+
+    # Create formatters and add it to handlers
+    console_format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+    file_format = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    console_handler.setFormatter(console_format)
+    file_handler.setFormatter(file_format)
+
+    # Add handlers to the logger
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    return logger
+
+
+# calling the logger
+logger = create_logger()
 
 
 # logfunc
- def logfunc(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        try:
-            value = func(*args, **kwargs)
-        except Exception as e:
-            logging.exception(f'An Error Occured in {func.__name__} function. Check the logs')
-            value = None
-        return value
+def logfunc(_func=None, logger=logger):
+    """
+    A decorator that wraps the passed in function and logs
+    exceptions should one occur
 
-    return wrapper
+    @param logger: The logging object
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception:
+                # log the exception
+                err = "There was an exception in  "
+                err += func.__name__
+                logger.exception(err)
+
+                # re-raise the exception
+                raise
+
+        return wrapper
+
+    if _func is None:
+        return decorator
+    else:
+        return decorator(_func)
+
+
+
