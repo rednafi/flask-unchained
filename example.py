e1e1b9ed3d826fbdac7678309290dc0bc9f1079d
rednafi <redowan.nafi@gmail.com>
2019-10-28 17:55:40 +0600

    added timer function

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
-from pyfoster.custom_logger import logfunc
+from pyfoster.func_logger import logfunc, timefunc
 import sys
 import pyfoster
+
 
 @logfunc
 def zero_div(a, b=0):
     return a // b
-
 
 print(zero_div(5))
